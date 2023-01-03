8948dbce4e1e4cb55b1bbd521a4c1bc98252164a
beegeiger <beegeiger@gmail.com>
2023-01-03 14:39:23 -0800

    Filling out model.py from old template

---
+++ model.py
@@ -0,0 +1,135 @@
+"""Models and database functions for scoothub App"""
+from flask import jsonify, Flask
+import datetime
+from datetime import datetime
+import json
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from sqlalchemy import Column, ForeignKey, Integer, Unicode, inspect, create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from flask_debugtoolbar import DebugToolbarExtension
+
+# from server import app
+
+# Required to use Flask sessions and the debug toolbar
+engine = create_engine('sqlite:///:scoothub:', echo=True)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///scoothub'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy()
+db.app = app
+#######################
+
+
+class User(db.Model):
+	"""User Table in SafeWork App"""
+
+	__tablename__ = "users"
+
+	user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+	username = db.Column(db.String(64), nullable=True)
+	name = db.Column(db.String(64), nullable=True)
+	fname = db.Column(db.String(64), nullable=True)
+	lname = db.Column(db.String(64), nullable=True)
+	email = db.Column(db.String(256))
+	email2 = db.Column(db.String(256))
+	created_at = db.Column(db.DateTime, nullable=True)
+	timezone = db.Column(db.String(48))
+	phone = db.Column(db.String(28), nullable=True)
+	safe_code = db.Column(db.String(64), nullable=True)
+	danger_code = db.Column(db.String(64), nullable=True)
+
+	def __repr__(self):
+		"""Provide helpful representation when printed."""
+		return "<user_id={} username={} name={} fname={} lname={} email={} email2={} created_at={} timezone={} phone={} safe_code={} danger_code={}>".format(
+			self.user_id, self.username, self.name, self.fname, self.lname, self.email, self.email2, self.created_at, self.timezone, self.phone, self.safe_code, self.danger_code)
+
+
+class User_log(db.Model):
+	"""SafeWalk Contacts"""
+
+	__tablename__ = "user_log"
+
+	user_log_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+	datetime = db.Column(db.DateTime, nullable=True)
+	time = db.Column(db.Time, nullable=True)
+	type = db.Column(db.String(200), nullable=True)
+	l_message = db.Column(db.String(1028), nullable=True)
+	full_note = db.Column(db.String(2056), nullable=True)
+
+	def __repr__(self):
+		"""Provide helpful representation when printed."""
+		return "<user_log_id={} user_id={} datetime={} time={} type={} l_message={} full_note={}>".format(
+			self.user_log_id, self.user_id, self.datetime, self.time, self.type, self.l_message, self.full_note)
+
+
+
+class Source(db.Model):
+	"""scoothub sources"""
+
+	__tablename__ = "sources"
+
+	contact_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
+	name = db.Column(db.String(96))
+	email = db.Column(db.String(200), nullable=True)
+	phone = db.Column(db.String(48), nullable=True)
+	c_type = db.Column(db.String(48), nullable=True)
+	c_message = db.Column(db.String(1028), nullable=True)
+
+	def __repr__(self):
+		"""Provide helpful representation when printed."""
+		return "<contact_id={} user_id={} name={} email={} phone={} c_type={} c_message={}>".format(
+			self.contact_id, self.user_id, self.name, self.email, self.phone, self.c_type, self.c_message)
+
+
+################################################################################
+
+def starter_data():
+	"""A user and 2 associated contacts are added for the developer backdoor login. Function call should be deleted before deployment."""
+	#Developer User Created and Added to dBase
+	new_user = User(user_id=999, name='dev', email='developer@placeholder.com', username='dev', fname='Dev', lname='Eveloper', created_at=datetime.now(), timezone="-8")
+	db.session.add(new_user)
+	db.session.commit()
+	#Two placeholder contacts are created associated with developer user and added
+	contact1 = Contact(user_id = 999, name='Sneezy', email='Sneezy@placeholder.com', phone='5555555569')
+	contact2 = Contact(user_id = 999, name='Dopey', email='Dopey@placeholder.com', phone='5555555519')
+	db.session.add_all([contact1, contact2])
+	db.session.commit()
+	return
+
+def example_data():
+	"""Example data to be used for testing."""
+	#Deleting tables in case this file has been run before
+	User.query.delete()
+	Flag.query.delete()
+	#Example Users
+	u1 = User(password=bcrypt.hashpw("12356".encode(), bcrypt.gensalt()), username="LaceyKittey", fname="Lacey", lname="Kittey", email="lkitty@.com", created_at=datetime.now(), edited_at=datetime.now())
+	u2 = User(password=bcrypt.hashpw("abcdef".encode(), bcrypt.gensalt()), username="HappyDoc", fname="Happy", lname="Doc", email="HDoc@.com", created_at=datetime.now(), edited_at=datetime.now())
+	u3 = User(password=bcrypt.hashpw("Testing".encode(), bcrypt.gensalt()), username="Testing", fname="Dev", lname="Tester", email="Testing@gmail.com", created_at=datetime.now(), edited_at=datetime.now())
+	db.session.add_all([u1, u2, u3])
+	db.session.commit()
+	return
+# i = inspect(model)
+# i.relationships
+# referred_classes = [r.mapper.class_ for r in i.relationships]
+##############################################################################
+# Helper functions
+
+def connect_to_db(app, db_uri='postgresql:///besafe'):
+	"""Connect the database to our Flask app."""
+	# Configure to use our PstgreSQL database
+	print("Connecting")
+	# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	# db.app = app
+	db.init_app(app)
+	with app.app_context():
+		db.drop_all()
+		db.create_all()
+		starter_data()
+
+if __name__ == "__main__":
+	connect_to_db(app, 'postgresql:///besafe')
+	print("Connected to DB.")
